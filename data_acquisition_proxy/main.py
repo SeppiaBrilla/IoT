c9f9dfe3f4d6b3698b4302604238de61afcbe8bf
seppiabrilla <alessio.pellegrino@studio.unibo.it>
2024-08-09 14:02:56 +0200

    env configuration

--- data_acquisition_proxy/main.py
+++ data_acquisition_proxy/main.py
@@ -1,5 +1,6 @@
 import json
 import asyncio
+import os
 import threading
 from coap_server import start_coap_server
 from http_server import app
@@ -8,7 +9,13 @@ from mqtt_server import create_mqtt_server
 if __name__ == '__main__':
     # Initialize MQTT client
     # create_mqtt_server()
-    flask_thread = threading.Thread(target=lambda: app.run(host='localhost', port=5000))
+    host_address = os.environ.get("SERVER_ADDRESS")
+    host_port = os.environ.get("SERVER_PORT")
+    if host_address is None:
+        host_address = "localhost"
+    if host_port is None:
+        host_port = 5000
+    flask_thread = threading.Thread(target=lambda: app.run(host=host_address, port=int(host_port)))
     flask_thread.start()
 
     asyncio.run(start_coap_server())
